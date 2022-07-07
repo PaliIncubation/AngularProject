b243254199e47fc5188bb6ea3754b4d433f11466
sathya-1992 <nsathya.gce@gmail.com>
2022-07-07 16:26:38 +0530

    LeftPanel

--- TrackingProject/src/app/app.module.ts
+++ TrackingProject/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftPanelComponent
   ],
   imports: [
     BrowserModule,
